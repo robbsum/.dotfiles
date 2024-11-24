d08f20d1baf92db1ef06632bee564db541a16659
robbsum <robbsum@gmail.com>
2024-11-24 21:23:38 +0530

    dotfiles: user: firefox: Disable crappy home activity stream page

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -28,6 +28,25 @@
           "services.sync.prefs.sync.browser.newtabpage.activity-stream.section.highlights.includePocket" =
             false;
 
+          # Disable crappy home activity stream page
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts" = false;
+          "browser.newtabpage.blocked" = lib.genAttrs [
+            # Youtube
+            "26UbzFJ7qT9/4DhodHKA1Q=="
+            # Facebook
+            "4gPpjkxgZzXPVtuEoAL9Ig=="
+            # Wikipedia
+            "eV8/WsSLxHadrTL1gAxhug=="
+            # Reddit
+            "gLv0ja2RYVgxKdp0I5qwvA=="
+            # Amazon
+            "K00ILysCaEq8+bEqV/3nuw=="
+            # Twitter
+            "T9nJot5PurhJSy8n038xGA=="
+          ] (_: 1);
+
           # Layout
           "browser.uiCustomization.state" = builtins.toJSON {
             currentVersion = 20;
