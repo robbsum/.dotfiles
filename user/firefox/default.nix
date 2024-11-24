e7e32ef6b0a6545c48449c0f62714f9f5fc2324a
robbsum <robbsum@gmail.com>
2024-11-24 21:16:19 +0530

    dotfiles: user: firefox: Fully disable Pocket

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -6,10 +6,28 @@
   # Configure Firefox
   programs.firefox = {
     enable = true;
+    policies = {
+      DisablePocket = true;
+    };
     profiles = {
       luna = {
         isDefault = true;
         settings = {
+          # Fully disable Pocket
+          "extensions.pocket.enabled" = false;
+          "extensions.pocket.api" = "0.0.0.0";
+          "extensions.pocket.loggedOutVariant" = "";
+          "extensions.pocket.oAuthConsumerKey" = "";
+          "extensions.pocket.onSaveRecs" = false;
+          "extensions.pocket.onSaveRecs.locales" = "";
+          "extensions.pocket.showHome" = false;
+          "extensions.pocket.site" = "0.0.0.0";
+          "browser.newtabpage.activity-stream.pocketCta" = "";
+          "browser.newtabpage.activity-stream.section.highlights.includePocket" =
+            false;
+          "services.sync.prefs.sync.browser.newtabpage.activity-stream.section.highlights.includePocket" =
+            false;
+
           # Auto install extensions
           "extensions.autoDisableScopes" = 0;
         };
