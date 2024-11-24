d904c13d6542db3196c9cd3d23beb7ee25dfaa42
robbsum <robbsum@gmail.com>
2024-11-24 19:14:55 +0530

    dotfiles: user: neovim: Set keymap to quickly open Oil

--- user/neovim/config/lua/plugins/oil.lua
+++ user/neovim/config/lua/plugins/oil.lua
@@ -5,5 +5,6 @@ return {
         watch_for_changes = true,
         default_file_explorer = true,
         skip_confirm_for_simple_edits = true
-    }
+    },
+    keys = {{"-", "<CMD>Oil<CR>", mode = "n", desc = "Open parent directory"}}
 }
