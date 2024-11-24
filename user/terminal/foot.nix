32841fdb552b67d55a2b8ea1cd072d277a64f826
robbsum <robbsum@gmail.com>
2024-11-24 11:39:58 +0530

    dotfiles: user: foot: Set font family to Sauce Code Pro Nerd Font

--- user/terminal/foot.nix
+++ user/terminal/foot.nix
@@ -1,10 +1,23 @@
-{
+let
+  # Font family
+  font = "SauceCodePro Nerd Font";
+
+  # Font size
+  size = "10.7";
+in {
   # Configure Foot
   programs.foot = {
     enable = true;
     settings = {
       main = {
         term = "xterm-256color";
+
+        font = "${font}:size=${size}:style=SemiBold";
+        font-bold = "${font}:size=${size}:style=Bold";
+        font-italic = "${font}:size=${size}:style=Italic";
+        font-bold-italic = "${font}:size=${size}:style=Bold,Italic";
+        line-height = 19;
+        underline-offset = 19;
       };
     };
   };
