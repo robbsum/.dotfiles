be42b993dd216c9bd43359b73af1e44c44cf692f
robbsum <robbsum@gmail.com>
2024-11-24 17:19:23 +0530

    dotfiles: user: neovim: Enable buffer like filesystem editing with stevearc/oil.nvim

---
+++ user/neovim/config/lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+    "stevearc/oil.nvim",
+    opts = {
+        delete_to_trash = true,
+        watch_for_changes = true,
+        default_file_explorer = true,
+        skip_confirm_for_simple_edits = true
+    }
+}
