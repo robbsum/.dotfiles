64ff42197f234c81b049a9ebea0c6c2cbf68d9d0
robbsum <robbsum@gmail.com>
2024-11-24 21:20:55 +0530

    dotfiles: user: firefox: Set toolbar layout

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -28,6 +28,22 @@
           "services.sync.prefs.sync.browser.newtabpage.activity-stream.section.highlights.includePocket" =
             false;
 
+          # Layout
+          "browser.uiCustomization.state" = builtins.toJSON {
+            currentVersion = 20;
+            newElementCount = 5;
+            dirtyAreaCache = ["nav-bar" "PersonalToolbar" "toolbar-menubar" "TabsToolbar" "widget-overflow-fixed-list"];
+            placements = {
+              PersonalToolbar = ["personal-bookmarks"];
+              TabsToolbar = ["tabbrowser-tabs" "new-tab-button" "alltabs-button"];
+              nav-bar = ["back-button" "forward-button" "stop-reload-button" "urlbar-container" "downloads-button" "touch-vpn_anchorfree_com-browser-action" "unified-extensions-button" "reset-pbm-toolbar-button"];
+              toolbar-menubar = ["menubar-items"];
+              unified-extensions-area = [];
+              widget-overflow-fixed-list = [];
+            };
+            seen = ["save-to-pocket-button" "developer-button" "ublock0_raymondhill_net-browser-action" "touch-vpn_anchorfree_com-browser-action" "sponsorblocker_ajay_app-browser-action" "_testpilot-containers-browser-action"];
+          };
+
           # Auto install extensions
           "extensions.autoDisableScopes" = 0;
         };
