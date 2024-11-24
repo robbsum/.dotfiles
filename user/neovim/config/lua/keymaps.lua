bac6a738d3795e004cf19d3b9beb672c70e8d108
robbsum <robbsum@gmail.com>
2024-11-24 21:28:40 +0530

    dotfiles: user: neovim: Set keymaps to manage buffers

--- user/neovim/config/lua/keymaps.lua
+++ user/neovim/config/lua/keymaps.lua
@@ -1,1 +1,6 @@
 vim.g.mapleader = " "
+
+-- Manage buffers
+vim.keymap.set("n", "<Tab>", "<CMD>bn<CR>", {silent = true})
+vim.keymap.set("n", "<S-Tab>", "<CMD>bp<CR>", {silent = true})
+vim.keymap.set("n", "<C-q>", "<CMD>bd<CR>", {silent = true})
