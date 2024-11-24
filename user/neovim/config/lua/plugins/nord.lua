ab614ee5da1158bd457d6c9788feae7ffbad4081
robbsum <robbsum@gmail.com>
2024-11-24 14:31:22 +0530

    dotfiles: user: neovim: Enable nord colorscheme with shaunsingh/nord.nvim

---
+++ user/neovim/config/lua/plugins/nord.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "shaunsingh/nord.nvim",
+        config = function()
+            -- Nord config
+            vim.g.nord_bold = false
+            vim.g.nord_italic = false
+            vim.g.nord_borders = false
+            vim.g.nord_contrast = true
+            vim.g.nord_disable_background = true
+            vim.g.nord_uniform_diff_background = true
+
+            -- Load the colorscheme
+            require('nord').set()
+        end
+    }
+}
